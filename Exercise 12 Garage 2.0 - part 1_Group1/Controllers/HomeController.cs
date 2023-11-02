81340afe4ed376672fd5e3ba998d00baed41a933
CamillaRidstrom <camillaeridstrom@outlook.com>
2023-11-02 10:45:37 +0100

    Add project files.

---
+++ Exercise 12 Garage 2.0 - part 1_Group1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Exercise_12_Garage_2._0___part_1_Group1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Exercise_12_Garage_2._0___part_1_Group1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Exercise 12 Garage 2.0 - part 1_Group1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Exercise_12_Garage_2._0___part_1_Group1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
