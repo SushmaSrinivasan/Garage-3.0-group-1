3f39f46bd176ab578df635612f306c84606e08b6
Erik Lagnefeldt <e.lagnefeldt@gmail.com>
2023-11-06 09:49:14 +0100

    Added Cost, Rounded Down Hours and Minuts. Fixed overview display to only display desired information. Timestamp function fully functional.

---
+++ Exercise 12 Garage 2.0 - part 1_Group1/Models/ParkVehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Exercise_12_Garage_2._0___part_1_Group1.Models
+{
+    public class ParkVehicle
+    {
+        [Key] public int Id { get; set; }
+        public string RegistrationNumber { get; set; }
+        public DateTime ParkingDate { get; set; }
+        public VehicleType VehicleType { get; set; }
+        public string Color { get; set; } = string.Empty;
+        public string Brand { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public int NumberOfWheels { get; set; }
+
+        // Set ParkingDate to the current date and time
+        public ParkVehicle()
+        {
+            ParkingDate = DateTime.Now;
+        }
+    }
+}
