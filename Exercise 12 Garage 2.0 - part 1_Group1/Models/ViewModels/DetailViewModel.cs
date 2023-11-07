7327f745ddb4d957a1a72f9c9688128add04678b
Rudolf Malmhäll <ryubcd14@outlook.es>
2023-11-07 16:44:10 +0100

    Moved validation to ParkVehicle

--- Exercise 12 Garage 2.0 - part 1_Group1/Models/ViewModels/DetailViewModel.cs
+++ Exercise 12 Garage 2.0 - part 1_Group1/Models/ViewModels/DetailViewModel.cs
@@ -12,13 +12,10 @@ namespace Exercise_12_Garage_2._0___part_1_Group1.Models.ViewModels
 
         public string Color { get; set; } = string.Empty;
 
-        [StringLength(15)]
         public string Brand { get; set; } = string.Empty;
 
-        [StringLength(20)]
         public string Model { get; set; } = string.Empty;
 
-        [Range(0, 10, ErrorMessage = "Wheels value should be within 0 and 10")]
         public int NumberOfWheels { get; set; }
 
 
