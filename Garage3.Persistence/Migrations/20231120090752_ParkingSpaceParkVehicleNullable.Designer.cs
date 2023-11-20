492654e80d98ddb704122b74b9f7a76a1d855888
Rudolf Malmhäll <ryubcd14@outlook.es>
2023-11-20 10:10:32 +0100

    Change ParkVehicle property to nullable in ParkingSpace.

--- Garage3.Persistence/Migrations/20231120090752_ParkingSpaceParkVehicleNullable.Designer.cs
+++ Garage3.Persistence/Migrations/20231120090752_ParkingSpaceParkVehicleNullable.Designer.cs
@@ -4,6 +4,7 @@ using Garage3.Persistence.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Garage3.Persistence.Migrations
 {
     [DbContext(typeof(GarageContext))]
-    partial class GarageContextModelSnapshot : ModelSnapshot
+    [Migration("20231120090752_ParkingSpaceParkVehicleNullable")]
+    partial class ParkingSpaceParkVehicleNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Garage3.Persistence/Migrations/20231120090752_ParkingSpaceParkVehicleNullable.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Garage3.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ParkingSpaceParkVehicleNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ParkingSpaces_ParkVehicle_ParkVehicleId",
+                table: "ParkingSpaces");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ParkVehicleId",
+                table: "ParkingSpaces",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ParkingSpaces_ParkVehicle_ParkVehicleId",
+                table: "ParkingSpaces",
+                column: "ParkVehicleId",
+                principalTable: "ParkVehicle",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ParkingSpaces_ParkVehicle_ParkVehicleId",
+                table: "ParkingSpaces");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ParkVehicleId",
+                table: "ParkingSpaces",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ParkingSpaces_ParkVehicle_ParkVehicleId",
+                table: "ParkingSpaces",
+                column: "ParkVehicleId",
+                principalTable: "ParkVehicle",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
