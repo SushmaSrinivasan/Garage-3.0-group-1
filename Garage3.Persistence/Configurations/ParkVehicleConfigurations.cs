2393c074defb7cebd428d2ff5952bed8ace05352
sushmasrinivasan04@gmail.com <sushmasrinivasan04@gmail.com>
2023-11-20 15:42:41 +0100

    added DB seed and its services

---
+++ Garage3.Persistence/Configurations/ParkVehicleConfigurations.cs
@@ -0,0 +1,19 @@
+﻿using Garage3.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage3.Persistence.Configurations
+{
+    public class ParkVehicleConfigurations : IEntityTypeConfiguration<ParkVehicle>
+    {
+        public void Configure(EntityTypeBuilder<ParkVehicle> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
